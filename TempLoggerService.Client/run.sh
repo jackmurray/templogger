6b3e9db3ea55353d8f3f03a60dc8195c8390ffb4
Jack Murray <jack@c0rporation.com>
2016-07-20 23:39:48 +0100

    add shell script to get drive temperatures

---
+++ TempLoggerService.Client/run.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+for dev in `ls /dev/disk/by-partlabel`;
+do
+        TEMPERATURE=`/usr/sbin/smartctl -A /dev/disk/by-partlabel/$dev | grep ^194 | awk '{print $10}'`
+        /usr/bin/mono /root/temploggerclient/TempLoggerService.Client.exe --set $dev $TEMPERATURE
+done
