4a52c8a0dd89799eaa6532fe9b09f9c5bafc2d38
Jack Murray <jack@c0rporation.com>
2016-07-20 23:15:22 +0100

    refactoring

--- TempLoggerService.Client/Program.cs
+++ TempLoggerService.Client/Program.cs
@@ -15,61 +15,71 @@ namespace TempLoggerService.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static HttpClient client;
+
+        private static Guid GetDevice(string name)
         {
-            using (var client = new HttpClient())
+            Guid id = Guid.Empty;
+            HttpResponseMessage response = client.GetAsync(String.Format("api/device/{0}", name)).Result;
+            if (response.StatusCode == HttpStatusCode.NotFound)
             {
-                // New code:
-                client.BaseAddress = new Uri("http://core2/TempLoggerService/");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                string hostname = Dns.GetHostName();
-                Console.WriteLine("Hostname: {0}", hostname);
-                Guid id = Guid.Empty;
-                HttpResponseMessage response = client.GetAsync(String.Format("api/device/{0}", hostname)).Result;
-                if (response.StatusCode == HttpStatusCode.NotFound)
-                {
-                    HttpResponseMessage createresp = client.PostAsJsonAsync("api/device/", hostname).Result;
-                    if (createresp.StatusCode != HttpStatusCode.OK)
-                        throw new Exception("Unable to create device: " + createresp.StatusCode);
-                    else
-                        id = createresp.Content.ReadAsAsync<Guid>().Result;
-                }
-                else if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    id = response.Content.ReadAsAsync<Guid>().Result;
-                }
+                HttpResponseMessage createresp = client.PostAsJsonAsync("api/device/", name).Result;
+                if (createresp.StatusCode != HttpStatusCode.OK)
+                    throw new Exception("Unable to create device: " + createresp.StatusCode);
                 else
-                    throw new Exception("Unable to fetch device GUID");
-
-                if (id == Guid.Empty)
-                    throw new Exception("For some reason we still failed to get the device GUID.");
-
-                while (true)
-                {
-                    TempEntry n = new TempEntry()
-                    {
-                        temp = GetTemp(),
-                        device = id,
-                        timestamp = DateTime.UtcNow
-                    };
-
-                    try
-                    {
-                        HttpResponseMessage postresp = client.PostAsJsonAsync("api/temperature/LogTemp", n).Result;
-                        if (!postresp.IsSuccessStatusCode)
-                            throw new Exception("Failed to log temperature.");
-
-                        Thread.Sleep(30000);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                        Console.WriteLine(ex.StackTrace);
-                    }
-                }
+                    id = createresp.Content.ReadAsAsync<Guid>().Result;
             }
+            else if (response.StatusCode == HttpStatusCode.OK)
+            {
+                id = response.Content.ReadAsAsync<Guid>().Result;
+            }
+            else
+                throw new Exception("Unable to fetch device GUID");
+
+            if (id == Guid.Empty)
+                throw new Exception("For some reason we still failed to get the device GUID.");
+
+            return id;
+        }
+
+        private static void Setup()
+        {
+            client = new HttpClient();
+
+            // New code:
+            client.BaseAddress = new Uri("http://core2/TempLoggerService/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        static void Main(string[] args)
+        {          
+            string hostname = Dns.GetHostName();
+            Console.WriteLine("Hostname: {0}", hostname);
+            Guid id = GetDevice(hostname);           
+
+            while (true)
+            {
+                TempEntry n = new TempEntry()
+                {
+                    temp = GetTemp(),
+                    device = id,
+                    timestamp = DateTime.UtcNow
+                };
+
+                try
+                {
+                    HttpResponseMessage postresp = client.PostAsJsonAsync("api/temperature/LogTemp", n).Result;
+                    if (!postresp.IsSuccessStatusCode)
+                        throw new Exception("Failed to log temperature.");
+
+                    Thread.Sleep(30000);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }            
         }
 
         private static decimal GetTemp()
