46176d97c539002b4d654a1c20ff894cc8c67849
Jack Murray <jack@c0rporation.com>
2016-07-21 17:36:51 +0100

    add dropdown list

--- TempLoggerService/Controllers/HomeController.cs
+++ TempLoggerService/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Objects.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +12,30 @@ namespace TempLoggerService.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.devices = GetDevices();
             return View();
         }
+
+        protected IEnumerable<SelectListItem> GetDevices()
+        {
+            var ent = new temperaturelogEntities();
+            var devs = ent
+                        .devices
+                        .Select(x =>
+                                new
+                                {
+                                    id = x.deviceID,
+                                    name = x.deviceName
+                                });
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            foreach (var dev in devs)
+            {
+                items.Add(new SelectListItem { Value = dev.id.ToString(), Text = dev.name});
+            }
+
+            return new SelectList(items, "Value", "Text");
+        }
+
     }
 }
