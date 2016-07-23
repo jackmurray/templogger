07880a29ee39099cc68a6fd4a41a3dd2eed92192
Jack Murray <jack@c0rporation.com>
2016-07-23 18:53:54 +0100

    grab cpu temp as well

--- TempLoggerService.Client/run.sh
+++ TempLoggerService.Client/run.sh
@@ -4,3 +4,5 @@ do
         TEMPERATURE=`/usr/sbin/smartctl -A /dev/disk/by-partlabel/$dev | grep ^194 | awk '{print $10}'`
         /usr/bin/mono /root/temploggerclient/TempLoggerService.Client.exe --set $dev $TEMPERATURE
 done
+CPUTEMP=`sensors | grep Physical | awk '{print $4}' | grep -E -o "[0-9.]+"`
+/usr/bin/mono /root/temploggerclient/TempLoggerService.Client.exe --set nas4free_cpu $CPUTEMP
