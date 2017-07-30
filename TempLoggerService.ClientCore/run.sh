4843d2d598d45ec60e5ed64415dd61a2dd1733fa
Jack Murray <git@c0rporation.com>
2017-07-30 20:39:59 +0100

    update run script

--- TempLoggerService.ClientCore/run.sh
+++ TempLoggerService.ClientCore/run.sh
@@ -2,7 +2,7 @@
 for dev in `ls /dev/disk/by-partlabel`;
 do
         TEMPERATURE=`/usr/sbin/smartctl -A /dev/disk/by-partlabel/$dev | grep ^194 | awk '{print $10}'`
-        /usr/bin/mono /root/temploggerclient/TempLoggerService.Client.exe --set $dev $TEMPERATURE
+        /root/TempLoggerService.ClientCore/dotnet/dotnet /root/TempLoggerService.ClientCore/client/TempLoggerService.ClientCore.dll  --set $dev $TEMPERATURE
 done
-CPUTEMP=`sensors | grep Physical | awk '{print $4}' | grep -E -o "[0-9.]+"`
-/usr/bin/mono /root/temploggerclient/TempLoggerService.Client.exe --set nas4free_cpu $CPUTEMP
+CPUTEMP=`sensors | grep Package | awk '{print $4}' | grep -E -o "[0-9.]+"`
+/root/TempLoggerService.ClientCore/dotnet/dotnet /root/TempLoggerService.ClientCore/client/TempLoggerService.ClientCore.dll --set nas4free_cpu $CPUTEMP
