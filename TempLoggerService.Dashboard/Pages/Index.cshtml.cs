21197166688ed6afbe9e26529c48476fd897c3ee
Jack Murray <jack@c0rporation.com>
2020-09-25 22:02:40 +0100

    Load the API server address from the config file and just dump it to the page for testing

--- TempLoggerService.Dashboard/Pages/Index.cshtml.cs
+++ TempLoggerService.Dashboard/Pages/Index.cshtml.cs
@@ -5,21 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace TempLoggerService.Dashboard.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IConfiguration _configuration;
+        public string ApiServer {get; private set;}
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ApiServer = _configuration.GetSection("ApiServer").Value;
         }
     }
 }
