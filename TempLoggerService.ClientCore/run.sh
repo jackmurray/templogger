5436e962a75cdb2f0b0354b8679b0c2a5442f5e3
Jack Murray <jack@c0rporation.com>
2020-12-26 22:52:39 +0000

    Update shell scripts to use the new URLs/binaries

--- TempLoggerService.ClientCore/run.sh
+++ TempLoggerService.ClientCore/run.sh
@@ -2,7 +2,7 @@
 for dev in `ls /dev/disk/by-partlabel`;
 do
         TEMPERATURE=`/usr/sbin/smartctl -A /dev/disk/by-partlabel/$dev | grep ^194 | awk '{print $10}'`
-        /root/TempLoggerService.ClientCore/dotnet/dotnet /root/TempLoggerService.ClientCore/client/TempLoggerService.ClientCore.dll  --set $dev $TEMPERATURE
+        /root/TempLoggerService.ClientCore/TempLoggerService.ClientCore --set $dev $TEMPERATURE
 done
 CPUTEMP=`sensors | grep Package | awk '{print $4}' | grep -E -o "[0-9.]+"`
-/root/TempLoggerService.ClientCore/dotnet/dotnet /root/TempLoggerService.ClientCore/client/TempLoggerService.ClientCore.dll --set nas4free_cpu $CPUTEMP
+/root/TempLoggerService.ClientCore/TempLoggerService.ClientCore --set nas4free_cpu $CPUTEMP
