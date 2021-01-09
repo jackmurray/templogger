6d539ea7092af91e993700d37202ea52ea519057
Jack <Jack@c0rporation.com>
2021-01-09 09:32:54 +0000

    Fix up scripts and line endings

--- TempLoggerService.ClientCore/dd-wrt.sh
+++ TempLoggerService.ClientCore/dd-wrt.sh
@@ -5,5 +5,5 @@ TEMP24=$((GETTEMP1/2+20))
 GETTEMP2=`wl -i eth2 phy_tempsense | awk '{ print $1}'`
 TEMP50=$((GETTEMP2/2+20))
 
-curl --cacert ca.crt -X POST https://app.corp.c0rporation.com:8080/templogger/api/temperature -d "{\"Value\": \"$TEMP24\", \"DeviceId\": \"825295CD-8585-4BAD-8F1A-22FD58ED8E8F\"}" --header "Content-Type: application/json"
-curl --cacert ca.crt -X POST https://app.corp.c0rporation.com:8080/templogger/api/temperature -d "{\"Value\": \"$TEMP50\", \"DeviceId\": \"315D4DDB-49CF-43E2-9860-9581C1EE21FB\"}" --header "Content-Type: application/json"
+curl --cacert /jffs/ca.crt -X POST https://app.corp.c0rporation.com:8080/templogger/api/temperature -d "{\"Value\": $TEMP24, \"DeviceId\": \"825295CD-8585-4BAD-8F1A-22FD58ED8E8F\"}" --header "Content-Type: application/json"
+curl --cacert /jffs/ca.crt -X POST https://app.corp.c0rporation.com:8080/templogger/api/temperature -d "{\"Value\": $TEMP50, \"DeviceId\": \"315D4DDB-49CF-43E2-9860-9581C1EE21FB\"}" --header "Content-Type: application/json"
--- TempLoggerService.ClientCore/run.sh
+++ TempLoggerService.ClientCore/run.sh
@@ -1,8 +1,11 @@
 ﻿#!/bin/bash
 for dev in `ls /dev/disk/by-partlabel`;
 do
-        TEMPERATURE=`/usr/sbin/smartctl -A /dev/disk/by-partlabel/$dev | grep ^194 | awk '{print $10}'`
-        /root/TempLoggerService.ClientCore/TempLoggerService.ClientCore --set $dev $TEMPERATURE
+        RAW=`/usr/sbin/smartctl -A /dev/disk/by-partlabel/$dev | grep ^194`
+        if [ $? -eq 0 ]; then
+                TEMPERATURE=`echo $RAW | awk '{print $10}'`
+                /root/TempLoggerService.ClientCore/TempLoggerService.ClientCore --set $dev $TEMPERATURE
+        fi
 done
 CPUTEMP=`sensors | grep Package | awk '{print $4}' | grep -E -o "[0-9.]+"`
 /root/TempLoggerService.ClientCore/TempLoggerService.ClientCore --set nas4free_cpu $CPUTEMP
